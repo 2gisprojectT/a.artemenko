2c222fdb489261b143ce6794d9080ae60759bfa6
artemenko-a-a <artemenko.nsk@mail.ru>
2015-04-06 19:52:06 +0600

    Пробовал использовать setUp

--- Lion/Test.py
+++ Lion/Test.py
@@ -2,27 +2,29 @@ __author__ = 'andrejartemenko'
 
 from unittest import TestCase
 from Lion import Lion
-from Lion import Info
-from Lion import Action
+#from Lion import Info
+#from Lion import Action
 import unittest
 
 
 class Lion_test(TestCase):
+    def setUp(self):
+        self.L = Lion()
 
     def test_init(self):
-        L = Lion()
-        self.assertTrue(L.hungry, 'Initial state is wrong')
+        # L = Lion()
+        self.assertTrue(self.L.hungry, 'Initial state is wrong')
 
     def test_get(self):
-        L = Lion()
-        self.assertTrue(L.get_state(), 'Initial state is wrong')
+        # L = Lion()
+        self.assertTrue(self.L.get_state(), 'Initial state is wrong')
 
     def test_change(self):
-        L = Lion()
-        L.change_state()
-        self.assertFalse(L.hungry, 'Initial state is wrong')
+        # L = Lion()
+        self.L.change_state()
+        self.assertFalse(self.L.hungry, 'Initial state is wrong')
 
-
+'''
 class Info_test(TestCase):
 
     def test_relations_hungry(self):
@@ -48,7 +50,7 @@ class Info_test(TestCase):
         self.assertIn('Антилопа', I.reaction_not_hungry, 'There is no Антилопа in reaction_not_hungry')
         self.assertIn('Дерево', I.reaction_not_hungry, 'There is no Дерево in reaction_not_hungry')
         self.assertIn('Охотник', I.reaction_not_hungry, 'There is no Охотник in reaction_not_hungry')
-
+'''
 
 if __name__ == '__main__':
     unittest.main()
