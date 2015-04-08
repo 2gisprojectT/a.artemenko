58618b321f12b6c598e78490dcc000401837b86d
artemenko-a-a <artemenko.nsk@mail.ru>
2015-04-08 14:30:05 +0600

    Переделаны тесты для нового Льва

---
+++ Lion/Test.py
@@ -0,0 +1,27 @@
+__author__ = 'andrejartemenko'
+
+from unittest import TestCase
+from Lion import Lion
+
+import unittest
+
+
+class Lion_test(TestCase):
+    def setUp(self):
+        self.L = Lion({('антилопа', 0): ('Лев съедает антилопу', 1)})
+
+    def test_init(self):
+        self.assertEqual(self.L.reactions, {('антилопа', 0): ('Лев съедает антилопу', 1)}, 'Инициализация работает некорректно')
+
+    def test_get(self):
+        self.L.set_state(1)
+        self.assertTrue(self.L.get_state(), 1, 'Метод get_state работает не корректно')
+
+    def test_set(self):
+        self.L.set_state(0)
+        self.assertEqual(self.L.state, 0, 'Метод set_state работает не корректно')
+
+
+if __name__ == '__main__':
+    unittest.main()
+    Lion_test.init_test()
